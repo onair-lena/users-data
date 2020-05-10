140589793a36b37c13cab1e62663e8a1917a421f
onair-lena <pechenko@bk.ru>
2020-05-10 19:56:21 +0300

    checked active

--- client/components/dummy-view.js
+++ client/components/dummy-view.js
@@ -5,7 +5,7 @@ import { bindActionCreators } from 'redux'
 import Head from './head'
 
 const Dummy = () => {
-  const [users, setUsers] = useState([]) // eslint-disable-line
+  const [users, setUsers] = useState([])
   const getUsers = () =>
     axios('https://randomuser.me/api/?results=10').then((item) => {
       return setUsers(item.data.results)
@@ -14,21 +14,27 @@ const Dummy = () => {
     getUsers()
   }, [])
 
+  const [checked, getChecked] = useState(false)
+  const getInputChecked = () => {
+    return getChecked(!checked)
+  }
+  console.log(users)// eslint-disable-line
+
   return (
     <div className="w-3/4 border border-solid border-gray-800">
       <Head title="Hello" />
       <div className="items-center">
-        <div className="flex flex-row justify-around text-center">
-          <div className="border border-solid border-gray-800 min-w-56 text-black font-bold shadow-lg p-2 m-2">
+        <div className="flex flex-row justify-around text-center bg-gray-300">
+          <div className="border border-solid border-gray-800 bg-gray-400 min-w-56 text-black font-bold shadow-lg p-2 m-2">
             <input type="checkbox" className="text-center" />
           </div>
-          <div className="border border-solid border-gray-800 min-w-150 text-black font-bold shadow-lg p-2 m-2">
+          <div className="border border-solid border-gray-800 bg-gray-400 min-w-150 text-black font-bold shadow-lg p-2 m-2">
             First Name
           </div>
-          <div className="border border-solid border-gray-800 min-w-200 text-black font-bold shadow-lg p-2 m-2">
+          <div className="border border-solid border-gray-800 bg-gray-400 min-w-200 text-black font-bold shadow-lg p-2 m-2">
             Second Name
           </div>
-          <div className="border border-solid border-gray-800 min-w-66 text-black font-bold shadow-lg p-2 m-2">
+          <div className="border border-solid border-gray-800 bg-gray-400 min-w-66 text-black font-bold shadow-lg p-2 m-2">
             Age
           </div>
         </div>
@@ -36,7 +42,7 @@ const Dummy = () => {
       {users.map((it) => (
         <div className="flex flex-row justify-around text-center">
           <div className="border border-solid border-gray-800 min-w-56 text-black font-bold shadow-lg p-2 m-2">
-            <input type="checkbox" />
+            <input type="checkbox" checked={checked} onChange={() => getInputChecked()} />
           </div>
           <div className="border border-solid border-gray-800 min-w-150 text-black font-bold shadow-lg p-2 m-2">
             {it.name.first}
@@ -49,8 +55,8 @@ const Dummy = () => {
           </div>
         </div>
       ))}
-      <div className="border border-solid border-gray-800 w-full text-black font-bold shadow-lg p-4">
-        1
+      <div className="bg-gray-300 w-full text-black font-bold shadow-lg p-4">
+        users checked: {111}
       </div>
     </div>
   )
